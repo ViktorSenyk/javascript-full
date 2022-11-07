44b4b7a5fa7f49112c89bc6427b7a1e384c3911b
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-11-07 21:21:56 +0200

    add task 4

---
+++ 21_document_object_model/4_task/index.js
@@ -0,0 +1,4 @@
+export const getTitle = () => document.querySelector('.title').textContent;
+export const getDescription = () => document.querySelector('.about').innerText;
+export const getPlans = () => document.querySelector('.plans').innerHTML;
+export const getGoal = () => document.querySelector('.goal').outerHTML;
