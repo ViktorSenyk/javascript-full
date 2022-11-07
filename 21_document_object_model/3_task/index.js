ed5474cbac41c6fc173d07e10fe28c1573ee37f4
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-11-07 21:10:38 +0200

    + fix

--- 21_document_object_model/3_task/index.js
+++ 21_document_object_model/3_task/index.js
@@ -1,10 +1,10 @@
 export function getItemsList() {
-  let elementsList = document.querySelectorAll('.tecnology');
+  let elementsList = document.querySelectorAll('.technology');
   console.dir(elementsList);
   return elementsList;
 }
 export function getItemsArray() {
   let elementsArray = document.querySelectorAll('.tool');
-  console.dir(Array.from(elementsArray));
-  return elementsArray;
+  console.dir(elementsArray);
+  return Array.from(elementsArray);
 }
