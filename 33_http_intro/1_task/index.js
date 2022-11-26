ebb58e6685f03515c54d081a7576856c1d832923
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-11-26 03:37:21 +0200

    add 33_http_intro => task 1

---
+++ 33_http_intro/1_task/index.js
@@ -0,0 +1,16 @@
+const buttonElem = document.querySelector('.name-form__btn');
+const avatarElem = document.querySelector('.user__avatar');
+const userNameElem = document.querySelector('.user__name');
+const userLocationElem = document.querySelector('.user__location');
+const inputElem = document.querySelector('.name-form__input');
+
+buttonElem.addEventListener('click', () =>
+  fetch(`https://api.github.com/users/${inputElem.value}`).then(response =>
+    response.json().then(data => {
+      const { avatar_url, name, location } = data;
+      avatarElem.src = avatar_url;
+      userNameElem.textContent = name;
+      userLocationElem.textContent = location;
+    }),
+  ),
+);
